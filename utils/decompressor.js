666df279b60613caa75bda762f4cecf128471334
Victor Orlunda <vicorlunda@gmail.com>
2021-10-23 19:52:27 +0100

    create function to decompress multiple compressed files

--- utils/decompressor.js
+++ utils/decompressor.js
@@ -10,3 +10,14 @@ const decompressSingle = async filename => {
 
   await promisify(pipeline)(readStream, createGunzip(), writeStream);
 };
+
+const decompressMultiple = async filenames => {
+  for (const filename of filenames) {
+    const readsStream = fs.createReadStream(filename);
+    const writeStream = fs.createWriteStream(path.parse(filename).name);
+
+    await promisify(pipeline)(readsStream, createGunzip(), writeStream);
+  }
+};
+
+module.exports = { decompressSingle, decompressMultiple };
