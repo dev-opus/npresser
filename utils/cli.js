e5265e71cb41e86223f72126485ab4a1a97a68d8
Victor Orlunda <vicorlunda@gmail.com>
2021-10-23 16:04:55 +0100

    add new commands and their descriptions

--- utils/cli.js
+++ utils/cli.js
@@ -2,45 +2,47 @@ const meow = require('meow');
 const meowHelp = require('cli-meow-help');
 
 const flags = {
-	clear: {
-		type: `boolean`,
-		default: true,
-		alias: `c`,
-		desc: `Clear the console`
-	},
-	noClear: {
-		type: `boolean`,
-		default: false,
-		desc: `Don't clear the console`
-	},
-	debug: {
-		type: `boolean`,
-		default: false,
-		alias: `d`,
-		desc: `Print debug info`
-	},
-	version: {
-		type: `boolean`,
-		alias: `v`,
-		desc: `Print CLI version`
-	}
+  clear: {
+    type: `boolean`,
+    default: true,
+    alias: `c`,
+    desc: `Clear the console`,
+  },
+  noClear: {
+    type: `boolean`,
+    default: false,
+    desc: `Don't clear the console`,
+  },
+  debug: {
+    type: `boolean`,
+    default: false,
+    alias: `d`,
+    desc: `Print debug info`,
+  },
+  version: {
+    type: `boolean`,
+    alias: `v`,
+    desc: `Print CLI version`,
+  },
 };
 
 const commands = {
-	help: { desc: `Print help info` }
+  help: { desc: `Print help info` },
+  press: { desc: `Compress a file or multiple files` },
+  unpress: { desc: 'Decompress a file or multiple files' },
 };
 
 const helpText = meowHelp({
-	name: `npresser`,
-	flags,
-	commands
+  name: `npresser`,
+  flags,
+  commands,
 });
 
 const options = {
-	inferType: true,
-	description: false,
-	hardRejection: false,
-	flags
+  inferType: true,
+  description: false,
+  hardRejection: false,
+  flags,
 };
 
 module.exports = meow(helpText, options);
