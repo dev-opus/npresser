57820dc86490547dbb481f12efef98abf59fc60f
Victor Orlunda <vicorlunda@gmail.com>
2021-10-23 16:33:17 +0100

    update log function to take in optional message

--- utils/log.js
+++ utils/log.js
@@ -1,18 +1,18 @@
 const alert = require('cli-alerts');
 const chalk = require('chalk');
 
-module.exports = type => {
+module.exports = (type, msg) => {
   switch (type) {
     case 'error':
       alert({
         type: 'error',
-        msg: chalk.red('Operation was unsuccessful!'),
+        msg: msg || chalk.red('Operation was unsuccessful!'),
       });
       break;
     case 'success':
       alert({
         type: 'success',
-        msg: chalk.green('Operation was successful!'),
+        msg: msg || chalk.green('Operation was successful!'),
       });
       break;
     default:
